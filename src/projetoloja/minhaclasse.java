0c9bc700c9e185d75941d3af41e3b791d2b16bc2
vitor <vitortube013@gmail.com>
2023-10-07 01:55:17 -0400

    ajuste de programa

---
+++ src/projetoloja/minhaclasse.java
@@ -0,0 +1,15 @@
+package projetoloja;
+
+import java.util.Scanner;
+
+public class minhaclasse {
+
+	public static void main(String[] args) {
+		Scanner teclado = new Scanner(System.in );
+		System.out.println("Didite um valor inteiro:");
+		int valor;
+		valor =teclado.nextInt();
+		System.out.println("Valor digitado: " + valor);
+	}
+
+}
